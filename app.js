00f7aa7c50ed7970aff363e0dbd71103118223a4
pablok2 <pablok2@users.noreply.github.com>
2021-02-27 21:45:49 -0500

    Added sending code
    It has invalid addresses of course

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const Web3 = require('web3');
+
+const sendHp = () => {
+    let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+
+    web3.eth.sendTransaction({from: '0x12344321', data: '0x43211234'})
+    .once('sending', function(payload){ console.log('sending ' + payload); })
+    .once('sent', function(payload){ console.log('sent ' + payload); })
+    .once('transactionHash', function(hash){ console.log('transactionHash ' + hash); })
+    .once('receipt', function(receipt){ console.log('receipt ' + receipt); })
+    .on('confirmation', function(confNumber, receipt, latestBlockHash){ console.log('confNumber ' + confNumber + ' receipt ' + receipt + ' latestBlockHash ' + latestBlockHash); })
+    .on('error', function(error){ console.log('error ' + error); })
+    .then(function(receipt){
+        // will be fired once the receipt is mined
+        console.log('receipt ' + receipt);
+    });
+};
+
 console.log('Hello World');
+sendHp();
